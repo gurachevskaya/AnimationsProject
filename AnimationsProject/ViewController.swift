c523533fc1551c1001dbccbdf6df71cc574089df
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2022-12-20 19:08:31 +0100

    Project init

---
+++ AnimationsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsProject
+//
+//  Created by Karina gurachevskaya on 20.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
